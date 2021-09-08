f072f544f41b5cb50f2e343330eb6ab04550a070
yangkuo <kuo.yang@niu.com>
2021-09-08 14:41:36 +0800

    uniapp

---
+++ src/pages/home/home.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-09-08 13:40:57
+ * @LastEditTime: 2021-09-08 14:01:38
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /my-project/src/pages/home/home.vue
+-->
+<template>
+	<view class="content">
+		<view>{{title}}1234</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'home'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,49 +1,64 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-09-06 16:21:42
+ * @LastEditTime: 2021-09-08 13:53:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /my-project/src/pages/index/index.vue
+-->
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view class="content">
+    <image class="logo" src="/static/logo.png"></image>
+    <view>
+      <text @click="goto" class="title">{{ title }}</text>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+  data() {
+    return {
+      title: "Hello",
+    };
+  },
+  onLoad() {},
+  methods: {
+    goto() {
+      try {
+        console.log(1);
+        uni.navigateTo({
+          url: "/pages/home/home",
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  },
+};
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin: 200rpx auto 50rpx auto;
+}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+.text-area {
+  display: flex;
+  justify-content: center;
+}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
 </style>

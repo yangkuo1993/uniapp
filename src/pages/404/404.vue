b386a15ca1acd4ada7a894d2ff65bf9238b2139a
yangkuo <kuo.yang@niu.com>
2021-10-14 11:47:36 +0800

    底部导航条

---
+++ src/pages/404/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="content">
+    <view>{{ title }}</view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '404',
+    };
+  },
+  onLoad() {},
+  methods: {},
+};
+</script>
+
+<style></style>
---
+++ src/pages/order/order.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="content">
+    <view>{{ title }}</view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'order',
+    };
+  },
+  onLoad() {},
+  methods: {},
+};
+</script>
+
+<style></style>

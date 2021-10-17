5f954ca636bbfbd37140576b366660c818242b3c
yangkuo <kuo.yang@niu.com>
2021-10-17 12:49:51 +0800

    底部导航条

---
+++ src/pages/class/class.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="content">
+    <view>{{ title }}</view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'class',
+    };
+  },
+  onLoad() {},
+  methods: {},
+};
+</script>
+
+<style></style>
